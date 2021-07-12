be2c3453bc9bf376bfaef00cbad55506f6190cb5
Joseph Chingalo <profschingalo@gmail.com>
2021-07-12 10:38:01 +0300

    chore: sub implementing partners configs

--- configs/index.js
+++ configs/index.js
@@ -3,4 +3,5 @@ const { sourceServer } = require("./server-config");
 module.exports = {
   sourceConfig: sourceServer,
   implementingPartnerReferrence: "lcyyWZnfQNJ",
+  subImplementingPartnerReferrence: "k34JyGFpfT4",
 };
