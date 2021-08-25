8801c31c9af0f755306eb729b6f3a92da233f3f3
Joseph Chingalo <profschingalo@gmail.com>
2021-08-25 15:37:44 +0300

    chore: config for support service provider name's reference for  bulk update

--- configs/index.js
+++ configs/index.js
@@ -1,10 +1,11 @@
 const { sourceServer } = require("./server-config");
 
-//@TODO add reference for service provider
 module.exports = {
   sourceConfig: sourceServer,
   implementingPartnerDataElementReferrence: "lcyyWZnfQNJ",
   subImplementingPartnerDataElementReferrence: "k34JyGFpfT4",
   implementingPartnerAttributeReferrence: "klLkGxy328c",
   subImplementingPartnerAttributeReferrence: "fQInK8s2RNR",
+  serviceProviderDataElementReference: "GsWaSx1t3Qs",
+  serviceProviderAttributeReference: "DdnlE8kmIkT",
 };
